57479981eabf1ea664709b5ef140819caf6f3e68
fangxi <loveinori@qq.com>
2022-11-28 13:20:12 +0800

    user 项目

---
+++ odin-user-service/src/main/java/com/storyhasyou/odin/user/OdinUserApplication.java
@@ -0,0 +1,16 @@
+package com.storyhasyou.odin.user;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fangxi created by 2022/11/28
+ */
+@SpringBootApplication
+public class OdinUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdinUserApplication.class, args);
+    }
+
+}
