a89b67440bcc2fee61238a75b9b4bb6ec7b6aa6d
fangxi <loveinori@qq.com>
2022-11-30 15:08:28 +0800

    user 项目

---
+++ odin-user/odin-user-api/src/main/java/com/storyhasyou/odin/user/OdinUserApplication.java
@@ -0,0 +1,18 @@
+package com.storyhasyou.odin.user;
+
+import com.storyhasyou.kratos.annotation.EnableBladesOfChaos;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author fangxi created by 2022/11/30
+ */
+@EnableBladesOfChaos
+@SpringBootApplication
+public class OdinUserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OdinUserApplication.class, args);
+    }
+
+}
