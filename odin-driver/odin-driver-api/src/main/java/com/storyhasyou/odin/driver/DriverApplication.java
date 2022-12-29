33be826d2ebac631fe77d34c47e7e49693933b51
fangxi <loveinori@qq.com>
2022-12-29 15:45:45 +0800

    代驾项目。

--- odin-driver/odin-driver-api/src/main/java/com/storyhasyou/odin/driver/DriverApplication.java
+++ odin-driver/odin-driver-api/src/main/java/com/storyhasyou/odin/driver/DriverApplication.java
@@ -1,18 +1,18 @@
-package com.storyhasyou.odin.cloud;
+package com.storyhasyou.odin.driver;
 
 import com.storyhasyou.kratos.annotation.EnableBladesOfChaos;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * @author fangxi created by 2022/12/26
+ * @author fangxi created by 2022/12/29
  */
+@SpringBootApplication
 @EnableBladesOfChaos
-@SpringBootApplication(scanBasePackages = "com.storyhasyou.odin")
-public class OdinCloudApplication {
+public class DriverApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(OdinCloudApplication.class, args);
+        SpringApplication.run(DriverApplication.class, args);
     }
 
 }
